39344a9ddece8e30f244a0928da27b671da4f87b
Micke Nordin <kano@sunet.se>
2023-07-21 18:28:42 +0200

    Fix persistence

--- garage/scripts/entrypoint.sh
+++ garage/scripts/entrypoint.sh
@@ -1,21 +1,31 @@
-#!/bin/ash
+#!/bin/sh
 
-garage="/garage"
+PATH=/bin
+key=${S3_KEY}
+secret=${S3_SECRET}
+busybox --install -s /bin/
+ln -s /garage /bin/garage
 
-# Let's steal these from nextcloud example config
-key='EJ39ITYZEUH5BGWDRUFY'
-secret='M5MrXTRjkyMaxXPe2FRXMTfTfbKEnZCu+7uRTVSj'
+garage server &
+server_pid=${!}
+sleep  2s
 
-id=$(${garage} /garage status | /bin/busybox tail -1 |/bin/busybox awk '{print $1}')
-version=$(${garage} layout show | /bin/busybox grep "Current cluster layout version" | /bin/busybox awk -F ':' '{print $2}' | /bin/busybox sed 's/\s//')
-${garage} layout assign "${id}" -z nextcloud -c 10 -t docker
-${garage} layout apply --version $(( version + 1 ))
-${garage} layout show
-${garage} bucket create nextcloud
-${garage} key import -n nextcloud-app-key "${key}" "${secret}"
-${garage} bucket allow \
-  --read \
-  --write \
-  --owner \
-  nextcloud \
-  --key nextcloud-app-key
+if ! garage bucket list | grep nextcloud; then
+  id=$(garage status | tail -1 | awk '{print $1}')
+  version=$(garage layout show | grep "Current cluster layout version" | \
+    awk -F ':' '{print $2}' | sed 's/\s//')
+  garage layout assign "${id}" -z nextcloud -c 10 -t docker
+  garage layout apply --version $(( version + 1 ))
+  garage layout show
+  garage bucket create nextcloud
+  garage key import -n nextcloud-app-key "${key}" "${secret}"
+  garage bucket allow \
+    --read \
+    --write \
+    --owner \
+    nextcloud \
+    --key nextcloud-app-key
+fi
+kill ${server_pid}
+
+garage server
